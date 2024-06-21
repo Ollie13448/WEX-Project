8870b5539ceebaa485f0f0d3741c1ac9a688cc24
Olivia <77274252+Ollie13448@users.noreply.github.com>
2024-06-21 11:22:05 +0100

    Delete WEX-Project/Wex Project directory

--- WEX-Project/Wex Project/registration/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    user_id = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30)
-    surname = models.CharField(max_length=30)
-    password = models.CharField(max_length=50)
--- WEX-Project/Wex Project/registration/urls.py
+++
@@ -1,8 +0,0 @@
-# registration/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('success/', views.success, name='success'),
-]
